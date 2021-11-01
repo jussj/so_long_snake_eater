bb0789596c44dc8c463fcb9e38a5a1451775c9a2
Justine SAINT JALMES <jusaint-@e3r5p21.clusters.42paris.fr>
2021-11-01 17:29:49 +0100

    added map_inspector features in parser

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,11 +6,28 @@
 /*   By: jusaint- <jusaint-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 10:02:40 by jusaint-          #+#    #+#             */
-/*   Updated: 2021/10/31 17:18:33 by jusaint-         ###   ########.fr       */
+/*   Updated: 2021/11/01 11:52:46 by jusaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+/*
+int flood_fill(t_data *data)
+*/
+int is_map_valid(t_data *data)
+{
+	int he;
+	int wi;
+
+	he = ft_strlen((char *)data->scene->map);
+	wi = ft_strlen(data->scene->map[0]);
+	if (he < wi)
+	{
+		return (0);
+	}
+	return (1);
+
+}
 
 int inspect_map(t_data *data)
 {
@@ -18,6 +35,7 @@ int inspect_map(t_data *data)
 	int j;
 
 	i = 0;
+	is_map_valid(data);
 	while (data->scene->map[i])
 	{
 		j = 0;
@@ -27,11 +45,13 @@ int inspect_map(t_data *data)
 				data->scene->sprite++;
 			else if (data->scene->map[i][j] == EXIT)
 				data->scene->exit++;
+			else if (data->scene->map[i][j] == PLAYER)
+				data->scene->player++;
 			j++;
 		}
 		i++;
 	}
-	if (data->scene->sprite == 0 || data->scene->exit == 0)
+	if (data->scene->sprite < 1 || data->scene->exit < 1 || data->scene->player != 1)
 		return (1);
 	return (0);
 }
@@ -62,8 +82,6 @@ int get_map(char *line, t_data *data)
 	}
 }
 
-// identify C collectibles (at least one)
-// E map exit (at least one)
 // P player's starting position (at least one)
 // check whether map is closed and is rectangular
 
@@ -72,6 +90,7 @@ int parsing_scene(t_data *data, int ac, char **av)
 {
 	int i;
 	int fd;
+	int ret;
 	char *line;
 
 	(void)ac;
@@ -81,7 +100,9 @@ int parsing_scene(t_data *data, int ac, char **av)
 	get_map(line, data);
 	data->scene->map = ft_split(data->scene->tmp, '|');
 	free(data->scene->tmp);
-	inspect_map(data);
+	ret = inspect_map(data);
+	if (ret == 1)
+		exit(1);
 	printf("MAP:\n");
 	i = 0;
 	while (data->scene->map[i])
@@ -89,7 +110,5 @@ int parsing_scene(t_data *data, int ac, char **av)
 		printf("%s\n", data->scene->map[i]);
 		i++;
 	}
-	printf("SPRITE: %d\n", data->scene->sprite);
-	printf("EXIT: %d\n", data->scene->exit);
 	return (0);
 }
--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: jusaint- <jusaint-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:18:41 by jusaint-          #+#    #+#             */
-/*   Updated: 2021/10/31 17:44:57 by jusaint-         ###   ########.fr       */
+/*   Updated: 2021/11/01 15:55:26 by jusaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int		debug_info(t_data *data)
 
 //	sprintf(buff, "DIRECTIONS  X %2f Y %2f", data->player->dir_x, data->player->dir_y);
 	sprintf(buff, "COLLECTIBLES LEFT %2d", data->scene->sprite);
-	mlx_string_put(data->mlx_ptr, data->win_ptr, 10, 245, 0xFFFFFF, buff);
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 10, 265, 0xFFFFFF, buff);
 	sprintf(buff, "COORDINATES X %2f Y %2f", data->player->x, data->player->y);
-	mlx_string_put(data->mlx_ptr, data->win_ptr, 10, 260, 0xFFFFFF, buff);
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 10, 280, 0xFFFFFF, buff);
 	return (0);	
 }
